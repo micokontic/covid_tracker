70d9a1b4a8904c3cdb7004d1fef6bf17cf46d00f
Mico Kontic <micokontic@gmail.com>
2020-09-30 15:33:53 +0200

    Updates

---
+++ precache-manifest.285b059dcd461a8d8462f28d26060c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de61c1318cbc30ba1cc7acaf5087dabf",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "3a0b5af52e2f90a219c3",
+    "url": "/covid_tracker/static/css/main.c9154fde.chunk.css"
+  },
+  {
+    "revision": "6cf25f61906ca7222671",
+    "url": "/covid_tracker/static/js/2.67f307b8.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid_tracker/static/js/2.67f307b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0b5af52e2f90a219c3",
+    "url": "/covid_tracker/static/js/main.2d3fd53c.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
